14dae9899b09d1223ffbfc2817480072ae27c04e
F-BONAPARTA <67749770+bonaparta13@users.noreply.github.com>
2023-10-04 17:52:41 +0300

    update kotlin_version

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -2,6 +2,8 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
 #include <show_network_interface_info/show_network_interface_info_plugin.h>
